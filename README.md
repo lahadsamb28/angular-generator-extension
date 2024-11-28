54b649241d683b2747af3dd9f6d5393beaf43772
Abdou Lahad SAMB <100447448+lahadsamb28@users.noreply.github.com>
2024-11-28 14:50:56 +0000

    angular generator component done !

---
+++ README.md
@@ -0,0 +1,50 @@
+# Angular Component Generator README
+
+A Visual Studio Code extension to generate Angular components in a specified folder with a right-click context menu.
+
+## Features
+
+- Generate Angular components quickly and easily.
+- Right-click on a folder in the Explorer view to create a new Angular component.
+- Automatically integrates with Angular CLI to ensure components are created within the correct Angular workspace.
+
+## Installation
+
+1. Download the `.vsix` file of this extension.
+2. Open Visual Studio Code.
+3. Go to the Extensions view by clicking on the Extensions icon in the Activity Bar on the side of the window or by pressing `Ctrl+Shift+X`.
+4. Click on the `...` (More Actions) button.
+5. Select `Install from VSIX...`.
+6. Browse to the downloaded `.vsix` file and install.
+
+## Usage
+
+1. Right-click on any folder in the Explorer view.
+2. Select **Generate Angular Component** from the context menu.
+3. Enter the desired component name in the input box.
+4. The component will be generated in the specified folder.
+
+## Requirements
+
+- An Angular workspace must be set up.
+- Angular CLI must be installed globally.
+
+## Known Issues
+
+- Ensure you are within an Angular workspace when using the command to avoid errors.
+
+## Release Notes
+
+### 0.0.1
+
+- Initial release of Angular Component Generator.
+- Supports right-click context menu for generating Angular components.
+
+## Contributing
+
+Contributions are welcome! Please submit issues and pull requests for any improvements or fixes.
+
+## License
+
+MIT License
+
---
+++ src/extension.ts
@@ -0,0 +1,42 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import { exec } from 'child_process';
+import path from 'path';
+import * as fs from 'fs';
+import { stderr, stdout } from 'process';
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand('angular-component-generator.generateComponent', async (uri: vscode.Uri) =>{
+		const workspaceFolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+		const angularJsonPath = path.join(workspaceFolder || '', 'angular.json');
+		if(fs.existsSync(angularJsonPath)){
+			// const folderPath = await vscode.window.showInputBox({ prompt: 'Enter the path file'});
+			const componentName = await vscode.window.showInputBox({ prompt: 'Enter the component name'});
+			if(componentName) {
+				const folderPath = uri.fsPath;
+				const command = `ng generate component ${componentName} --path=${folderPath}`;
+	
+				exec(command, {cwd: workspaceFolder}, (err, stdout, stderr) => {
+					if(err) {
+						vscode.window.showErrorMessage(`Error: ${stderr}`);
+					}else{
+						vscode.window.showInformationMessage(`Component ${componentName} created successfully in ${folderPath}!`);
+					}
+				});
+	
+			}else{
+				vscode.window.showErrorMessage('Component name is missing');
+			}
+		}else{
+			vscode.window.showErrorMessage('you must run this command inside an angular workspace');
+		}		
+	});
+	context.subscriptions.push(disposable);
+	
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
